9fcefe358e57c91e05fb22586c4a4a38d568a690
dmoprates <prates.diego@gmail.com>
2025-10-15 14:16:13 -0300

    Feat: Configuração Inicial do Projeto, adequando e configurando os arquivos do sistema base

--- index.php
+++ index.php
@@ -4,15 +4,10 @@ Site::updateUsuarioOnline();
 Site::contador(); 
 
 $homeController = new controller\homeController();
-$finalizarController = new controller\finalizarController();
 
 Router::get('/',function() use ($homeController){
 	$homeController->index();
 });
-
-Router::get('/finalizar',function() use ($finalizarController){
-	$finalizarController->index();
-})
 
 
 ?>
